215661a61bbe66d3a64b0d0bec472316e7421673
Brenton Moodley <bmoodley@student.42.fr>
2019-01-04 23:26:19 +0200

    pushed notes.js for review

--- src/usingDB/controllers/notes.js
+++ src/usingDB/controllers/notes.js
@@ -1,65 +1,48 @@
 const queries		= require('./queries');
 const config		= require('../../../config');
 const users			= require('./user');
+const utils			= require('./utils');
 
 const db			= config.db;
-
-// note exists
-	function noteExists(note_id){
-		return new Promise((resolve, reject) => {
-			db.oneOrNone(queries.PQ_noteExists, [note_id])
-			.then(data => {
-				resolve(data);
-			})
-			.catch(err => {
-				reject(err);
-			})
-		})
-	}
 
 // create
 	function createNote(req, res){
 		db.none(queries.PQ_createNote, [req.body.user_id, req.body.note, req.body.date_created, false, null])
 		.then( function() {
-			res.status(200)
-			.json({
-				status: 'success',
-				message: 'created new note'
-			});
+			utils.resObj(res, 200, 'success', 'created new note', null);
 		})
 		.catch(error => {
 			console.log('ERROR:', error); // print the error
-			res.status(200)
-			.json({
-				status: 'fail',
-				message: 'error create note failed',
-				data: error
-			});
+			utils.resObj(res, 200, 'fail', 'error: failed to create note', error); // utils
 		})
 		.finally(db.end);
 	}
 
 // read
 	function getNotesByUser(req, res){
-		db.any(queries.PQ_getNotesByUser, [req.params.user_id])
+		utils.userExistsID(req.params.user_id)
 		.then(data => {
-			if (data.length){
-				res.status(200)
-				.json({
-					status: 'success',
-					message: 'Retrieved all notes by this user',
-					data: data
-				});
+			if (data){
+				db.any(queries.PQ_getNotesByUser, [req.params.user_id])
+				.then(data => {
+					if (data.length){
+						utils.resObj(res, 200, 'success', 'retrieved all notes by this user', data);
+					} else {
+						utils.resObj(res, 200, 'fail', 'no notes by this user exist', null);
+					}
+				})
+				.catch(error => { // IS THIS REDUNDANT?????? 
+					console.log('ERROR:', error); // print the error
+					utils.resObj(res, 200, 'fail', 'error: failed to get notes by user', error);
+				})
+				.finally(db.end);
 			} else {
-				res.status(200)
-				.json({
-					status: 'fail',
-					message: 'No notes by this user',
-				});
+				utils.resObj(res, 200, 'fail', 'user with that user_id does not exist', error);
 			}
 		})
 		.catch(error => {
 			console.log('ERROR:', error); // print the error
+			utils.resObj(res, 200, 'fail', 'error: failed to get notes by user', error);
 		})
 		.finally(db.end);
 	}
@@ -68,90 +51,65 @@ const db			= config.db;
 		db.any(queries.PQ_getAllNotes)
 		.then(data => {
 			if (data.length){
-				res.status(200)
-				.json({
-					status: 'success',
-					message: 'Retrieved all notes',
-					data: data
-				});
+				utils.resObj(res, 200, 'success', 'retreived all notes', data);
 			} else {
-				res.status(200)
-				.json({
-					status: 'fail',
-					message: 'No notes exist',
-				});
+				utils.resObj(res, 200, 'fail', 'no notes exist', null);
 			}
 		})
 		.catch(error => {
 			console.log('ERROR:', error); // print the error
+			utils.resObj(res, 200, 'fail', 'error: failed to get notes', error);
 		})
 		.finally(db.end);
 	}
 
 // update
 	function updateNote(req, res){
-		noteExists(req.body.note_id)
+		utils.noteExists(req.body.note_id)
 		.then(data => {
 			if (data){
 				console.log('note exists');
 				db.none(queries.PQ_updateNote, [req.body.note, req.body.date_edited, true, req.body.note_id])
 				.then(data => {
-					res.status(200)
-					.json({
-						status: 'success',
-						message: 'Updated note',
-					});
+					utils.resObj(res, 200, 'success', 'note updated', null);
 				})
 				.catch(error => {
 					console.log('Error:', error);
+					utils.resObj(res, 200, 'fail', 'note not updated', error);
 				})
 				.finally(db.end);
 			}
 			else {
-				res.status(200)
-				.json({
-					status: 'fail',
-					message: 'Note with specified note_id does not exist'
-				});
+				utils.resObj(res, 200, 'fail', 'note with specified note_id does not exist', null);
 			}
 		})
 		.catch(err => {
 			console.log('ERROR:', err); // print the error
+			utils.resObj(res, 200, 'fail', 'error: failed to create note', error);
 		})
 		.finally(db.end);
 	};
 
 // delete
 	function deleteNote(req, res){ // ADD CHECK FOR IF DELETION OCCURED AT ALL
-		noteExists(req.params.note_id)
+		utils.noteExists(req.params.note_id)
 		.then(data =>{
 			if (data){
 				db.result(queries.PQ_deleteNote, [req.params.note_id])
 				.then( result => {
 					if (result.rowCount){
-						res.status(200)
-							.json({
-							status: 'success',
-							message: `Removed ${result.rowCount} note`
-						});
+						utils.resObj(res, 200, 'success', 'note has been deactivated, to restore go to trash', null);
 					} else {
-						res.status(200)
-						.json({
-						status: 'fail',
-						message: 'Failed to delete note'
-						});
+						utils.resObj(res, 200, 'fail', 'failed to delte note', null);
 					}
 				})
 			} else {
-				res.status(200)
-				.json({
-				status: 'fail',
-				message: 'Note with specified note_id does not exist'
-				});
+				utils.resObj(res, 200, 'fail', 'note with specified note_id does not exist', null);
 			}
 		})
 		.catch(error => {
 			console.log('Error:', error);
+			utils.resObj(res, 200, 'fail', 'error: failed to delete note', error);
 		})
 		.finally(db.end);
 	}
